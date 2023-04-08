e4f9545ad59025ab1e0d12a71a4163a3c1b43c46
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-04-08 21:33:29 +0700

    first-day

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
-if (sum < 21) {
+let hasBlackJack = false;
+let itAlive = true;
+
+if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game! 😭");
+  itAlive = false;
 }
